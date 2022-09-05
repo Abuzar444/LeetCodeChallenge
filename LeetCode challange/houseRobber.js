3504d9224761a7266364326760991dbaaef5e870
Abuzar Dawar <84353190+Abuzar444@users.noreply.github.com>
2022-09-05 20:02:31 +0500

    Add files via upload

---
+++ LeetCode challange/houseRobber.js
@@ -0,0 +1,16 @@
+var rob = function (nums) {
+    return helper([], 0, nums);
+};
+
+var helper = function (dp, i, nums) {
+    if (i >= nums.length) return 0;
+    if (dp[i] === undefined) {
+        dp[i] = Math.max(
+            helper(dp, i + 1, nums),
+            nums[i] + helper(dp, i + 2, nums)
+        );
+    }
+    return dp[i];
+};
+
+console.log(rob([1, 2, 3, 1]))
