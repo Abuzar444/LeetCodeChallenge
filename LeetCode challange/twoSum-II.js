9f0b9c60f6caa9eed56c610713882cfe161514c0
Abuzar Dawar <84353190+Abuzar444@users.noreply.github.com>
2022-09-17 23:40:27 +0500

    Add files via upload

---
+++ LeetCode challange/twoSum-II.js
@@ -0,0 +1,13 @@
+var twoSum = function(numbers, target) {
+    let i=0, j=numbers.length-1;
+    
+    while(i<j){
+        if(numbers[i] + numbers[j] > target){
+            j--;
+        }
+        else if(numbers[i] + numbers[j] < target) i++;
+        else return [i+1, j+1];
+    }
+};
+
+console.log(twoSum([2,7,11,15], 9))
